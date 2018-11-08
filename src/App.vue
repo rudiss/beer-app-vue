192d50101c739d23811e63f79a0c6ac02c401ec7
rudis <rudislpp@gmail.com>
2018-11-08 00:20:18 -0200

    navbar

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,17 @@
   <v-app>
     <v-content>
       <v-container>
+        <template>
+          <v-toolbar dark>
+            <v-toolbar-side-icon></v-toolbar-side-icon>
+            <v-toolbar-title>Beer App</v-toolbar-title>
+            <v-spacer></v-spacer>
+            <v-toolbar-items class="hidden-sm-and-down">
+              <v-btn flat>Home</v-btn>
+              <v-btn flat>About</v-btn>
+            </v-toolbar-items>
+          </v-toolbar>
+        </template>
         <Carousel />
         <!-- <NavigationBar /> -->
 
